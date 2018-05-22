4d7edbcad53e9b8877242e52e2ed2caccd82f0df
Unknown <sushsilsharma.ss451@gmail.com>
2018-05-22 18:48:13 +0530

    added images in contact.html

--- howdy/views.py
+++ howdy/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from csv import reader
 
 
 # Create your views here.
@@ -11,11 +12,25 @@ class HomePageView(TemplateView):
 class AboutPageView(TemplateView):
     template_name = "about.html"
 
+
 class BlogPageView(TemplateView):
     template_name = "blog.html"
 
+
 class ContactPageView(TemplateView):
+    # Read the csv file coordinators.csv
+    with open("howdy/coordinators.csv", "r") as f:
+        r = reader(f)
+        rest = [row for row in r]
+        rest = rest[1:]  # Remove the top row: Name, Email, etc
+
     template_name = "contact.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['objects'] = self.rest
+        return context
+
 
 class ProjectsPageView(TemplateView):
     template_name = "projects.html"
