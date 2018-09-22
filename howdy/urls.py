5807efd7ac7569a8bbd4a56a113d69679ed1081e
Shivansh Saini <shivanshs9@gmail.com>
2018-09-22 12:32:15 +0530

    Initial commit of projects.

--- howdy/urls.py
+++ howdy/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^about$', views.AboutPageView.as_view()),
     url(r'^contact$', views.ContactPageView.as_view()),
     url(r'^posts', views.PostsPageView.as_view()),
-    url(r'^projects$', views.ProjectsPageView.as_view()),
-    url(r'^markdown', views.ShowMD.as_view()),
+    url(r'^projects$', views.ProjectsPageView.as_view(), name='list-projects'),
+    url(r'^markdown', views.ShowMD.as_view(), name='show-markdown'),
 ]
