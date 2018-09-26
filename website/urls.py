bcb8f08b5afbf6aad36bbfb9974c6e3480daa126
Akshay Sharma <contact@imakshay.com>
2018-09-27 00:19:58 +0530

    add index.html url

--- website/urls.py
+++ website/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 from website import views
 
 urlpatterns = [
-    url(r'^$', views.TimelinePageView.as_view()),
+    url(r'^$', views.HomePageView.as_view()),
+    url(r'^timeline$', views.TimelinePageView.as_view()),
     url(r'^about$', views.AboutPageView.as_view()),
     url(r'^contact$', views.ContactPageView.as_view()),
     url(r'^posts', views.PostsPageView.as_view()),
